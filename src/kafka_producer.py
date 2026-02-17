60755917fb8c42b0862c620d8af2371347ae6f0c
cdev-76 <jorgeenrique.fernand@asesormasorange.es>
2026-02-17 19:50:22 +0100

    JSON and Key serializer

--- src/kafka_producer.py
+++ src/kafka_producer.py
@@ -1,12 +1,15 @@
 from kafka import KafkaProducer
+import json
 
 # Objeto productor que se conecta al broker de kafka
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+                         key_serializer=str.encode)
 
 if __name__ == "__main__":
 
     # Método que manda el paquete de bytes al topic prueba-tfg
-    producer.send('prueba-tfg',b'world')
+    producer.send('prueba-tfg',key='usuario1',value={'nombre':'Pepito','apellidos':'Pepitez'})
     
     # El método flush fuerza el envio de paquetes al broker
     producer.flush() 
