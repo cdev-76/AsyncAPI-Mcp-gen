27141c5cf6ccee8c116d733259a4928b8f6f4622
cdev-76 <jorgeenrique.fernand@asesormasorange.es>
2026-02-17 20:03:25 +0100

    Send function parametrized

--- src/kafka_producer.py
+++ src/kafka_producer.py
@@ -1,17 +1,30 @@
 from kafka import KafkaProducer
 import json
 
-# Objeto productor que se conecta al broker de kafka
+# Producer object that conects to the kafka broker
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'),
                          key_serializer=str.encode)
 
-if __name__ == "__main__":
-
-    # Método que manda el paquete de bytes al topic prueba-tfg
-    producer.send('prueba-tfg',key='usuario1',value={'nombre':'Pepito','apellidos':'Pepitez'})
+def send(topic:str, message:dict, key:str):
+    '''
+    Docstring for send
     
-    # El método flush fuerza el envio de paquetes al broker
+    :param topic: Kafka topic where the package is sent
+    :type topic: str
+    :param message: Content of the message in .JSON format
+    :type message: dict
+    :param key: Key that identifies the package in order to be sent
+    :type key: str
+    '''
+    producer.send(topic,key=key,value=message)
+
+
+if __name__ == "__main__":
+    datos_user1 = {'name':'Jorge','surname':'Fernandez'}
+    send('prueba-tfg',datos_user1,'user1')
+    
+    # Forces the sending of the package, even if it is too small to be sent
     producer.flush() 
     
-    print("Enviado!")
+    print("Sent!")
